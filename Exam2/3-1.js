2f0c407ca3ad4779d6ee048b901f70b52fc29e12
qhui <liesel.hui@gmail.com>
2015-04-24 09:01:37 -0400

    draft

--- Exam2/3-1.js
+++ Exam2/3-1.js
@@ -1,5 +1,30 @@
 var data;
 d3.json("exam2.json", function(json) {
   data = json;
-  console.log(data);
+  var columns = Object.key(data[0]);
+  var table = d3.select("body").append("table");
+  var thead = table.append("thead");
+  var tbody = table.append("tbody");
+
+  thead.append("tr")
+      .selectAll("th")
+      .data(columns)
+      .enter()
+      .append("th")
+      .text(function(column){return column; });
+
+  var rows = tbody.selectAll("tr")
+      .data(data)
+      .enter()
+      .append("tr");
+
+  var cells = rows.selectAll("td")
+      .data(data)
+      .enter()
+      .append("td")
+      .html(function(d){
+        var value = [];
+        value = value.push(d.ItemNo, d.A, d.B, d.C, d.D, d.E, d.F, d.E);
+        return value;
+      });
 });
