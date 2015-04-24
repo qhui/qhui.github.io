33346a7b786afcca151f03c1a6eb44b93e94c869
qhui <liesel.hui@gmail.com>
2015-04-24 12:13:22 -0400

    ss

--- Exam2/3-1.js
+++ Exam2/3-1.js
@@ -32,8 +32,8 @@ d3.json("exam2.json", function(json) {
       .on("click", function(column){
         console.log(column);
         data.sort(sort_by(column, true, parseInt));
-        d3.select("tbody")
-          .exit().remove();
+        d3.select("td")
+          remove();
         updateData();
       });
 
